596007953eb4ccf5e49cb403bf087b77ce33c132
Nitin Singh <nitin.singh01@ad.infosys.com>
2020-11-04 18:40:02 +0530

    Project

---
+++ index.js
@@ -0,0 +1,24 @@
+var jsforce = require('jsforce');
+var USERNAME = "";
+var PASSWORD = "";
+var conn = new jsforce.Connection({
+ clientId: '',
+ clientSecret: '',
+ redirectUri: 'http://localhost:3000/oauth/_callback',
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Amazon Software License
+// http://aws.amazon.com/asl/
+ mode: 'single'
+});
+exports.handler = function(event, context, callback) {
+console.log("Hello");
+var conn = new jsforce.Connection({});
+conn.login(USERNAME, PASSWORD, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  console.log("Hello");
+  conn.streaming.topic("/event/OppCRUD__e").subscribe(function(message) {
+	console.log("Hello");
+    console.dir(message);
+  });
+});
+};
